50faeb6edfce89bf3e453aeea6d5f233598a577a
daveraskin <draskin1991@gmail.com>
2023-10-30 07:47:13 -0700

    update build

--- build.sh
+++ build.sh
@@ -1,8 +1,17 @@
 #!/usr/bin/env bash
-# exit on error
 set -o errexit
 
-poetry install
+echo "Creating a virtual env for both poetry and our packages..."
+python -m venv venv
 
-poetry run python manage.py collectstatic --no-input
-poetry run python manage.py migrate
+echo "Updating pip ;-)..."
+./venv/bin/python -m pip install --upgrade pip
+
+echo "Installing new/better version of poetry into our virtual env..."
+./venv/bin/pip install poetry==1.4.2
+
+echo "Installing our (non-dev) packages..."
+cd /opt/render/project/src
+./venv/bin/poetry install --without dev
+
+echo "Done"
