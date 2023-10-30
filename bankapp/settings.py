9c1a06e4fabd2226b1e7fa73172e3f88063e41ad
daveraskin <draskin1991@gmail.com>
2023-10-30 08:00:31 -0700

    update jwt signing key

--- bankapp/settings.py
+++ bankapp/settings.py
@@ -79,7 +79,7 @@ SIMPLE_JWT = {
     "UPDATE_LAST_LOGIN": False,
 
     "ALGORITHM": "HS256",
-    "SIGNING_KEY": settings.SECRET_KEY,
+    "SIGNING_KEY": os.getenv('DJANGO_SECRET_KEY'),
     "VERIFYING_KEY": "",
     "AUDIENCE": None,
     "ISSUER": None,
